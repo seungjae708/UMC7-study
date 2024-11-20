a8798e85399a2dce52502d049b1021d2d45ca531
seungjae708 <choesj365@gmail.com>
2024-11-21 01:02:25 +0900

    Feat : 응답 통일, 에러 핸들러

---
+++ src/main/java/umc/spring/controller/TempRestController.java
@@ -0,0 +1,28 @@
+package umc.spring.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import umc.spring.apiPayload.ApiResponse;
+import umc.spring.converter.TempConverter;
+import umc.spring.service.TempService.TempQueryService;
+import umc.spring.web.dto.TempResponse;
+
+@RestController
+@RequestMapping("/temp")
+@RequiredArgsConstructor
+public class TempRestController {
+    private final TempQueryService tempQueryService;
+    @GetMapping("/test")
+    public ApiResponse<TempResponse.TempTestDTO> testApi() {
+        return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
+    }
+    @GetMapping("/exception")
+    public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam("flag") Integer flag){
+        tempQueryService.CheckFlag(flag);
+        return ApiResponse.onSuccess(TempConverter.toTempExceptionDTO(flag));
+    }
+}
